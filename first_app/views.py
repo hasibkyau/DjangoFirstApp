e37bd8ab70040d647661f5f2e6e5f25566ddf379
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2022-12-09 21:55:46 +0600

    showing form values after submitting on html page

--- first_app/views.py
+++ first_app/views.py
@@ -21,4 +21,18 @@ def about(request):
 def form(request):
     new_form = forms.user_form()
     diction = {'test_form': new_form, 'heading1':"This form is creater with django library"}
+    
+    if request.method == 'POST':
+        new_form = forms.user_form(request.POST)
+
+        if new_form.is_valid():
+            user_name = new_form.cleaned_data['user_name']
+            user_dob = new_form.cleaned_data['user_dob']
+            user_email = new_form.cleaned_data['user_email']
+
+            diction.update({'user_name':user_name})
+            diction.update({'user_dob':user_dob})
+            diction.update({'user_email':user_email})
+            diction.update({'form_submitted':"Yest"})
+
     return render(request, 'first_app/form.html', context=diction)
