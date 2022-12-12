05b5d3a19ef1515246a4e850e438e10097e7b016
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2022-12-12 10:36:34 +0600

    added django validators

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,9 +1,22 @@
 from django import forms
+from django.core import validators
+
+def even_or_not(value):
+    if value%2 == 1:
+        raise forms.ValidationError("please insert an even number!")
 
 class user_form(forms.Form):
-    user_name = forms.CharField(label="Full Name", required=True, initial="", widget=forms.TextInput(
-        attrs= {'placeholder':'Enter Your Full Name'}
-    ))
+    number_field = forms.IntegerField(validators=[even_or_not])
+
+
+
+
+
+
+
+
+class user_form3(forms.Form):
+    name = forms.CharField(validators = [validators.MaxLengthValidator(10)])
 
     choices = (('male','Male'), ('female','Female'))
     sex = forms.ChoiceField(choices=choices, label="Sex", widget=forms.RadioSelect)
@@ -22,6 +35,22 @@ class user_form(forms.Form):
 
 
 
+class user_form2(forms.Form):
+    user_name = forms.CharField(label="Full Name", required=True, initial="", widget=forms.TextInput(
+        attrs= {'placeholder':'Enter Your Full Name'}
+    ))
+
+    choices = (('male','Male'), ('female','Female'))
+    sex = forms.ChoiceField(choices=choices, label="Sex", widget=forms.RadioSelect)
+    
+    
+    languages = (('english','English'), ('bangla','Bangla'))
+    language = forms.MultipleChoiceField(choices=languages, label="Language", widget=forms.CheckboxSelectMultiple)
+    
+    user_dob = forms.DateField(label="Date of Birth", widget=forms.TextInput(
+        attrs={'type':'date'}
+    ))
+    user_email = forms.EmailField(label="Email", required=True, )
 
 
 
--- first_app/views.py
+++ first_app/views.py
@@ -18,25 +18,43 @@ def contact(request):
 def about(request):
     return HttpResponse("<h1>This is About Page</h1> <a href='/first_app/'>Homepage</a> <a href='/first_app/contact/'>Contact</a>")
 
+
+
 def form(request):
     new_form = forms.user_form()
     diction = {'test_form': new_form, 'heading1':"This form is creater with django library"}
     
     if request.method == 'POST':
         new_form = forms.user_form(request.POST)
-
+        diction.update({'test_form':new_form})
         if new_form.is_valid():
-            user_name = new_form.cleaned_data['user_name']
-            user_dob = new_form.cleaned_data['user_dob']
-            user_email = new_form.cleaned_data['user_email']
-            sex = new_form.cleaned_data['sex']
-            language=new_form.cleaned_data['language']
-
-            diction.update({'user_name':user_name})
-            diction.update({'user_dob':user_dob})
-            diction.update({'user_email':user_email})
-            diction.update({'sex':sex})
-            diction.update({'language':language})
+            diction.update({'test_form':new_form})
             diction.update({'form_submitted':"Yest"})
 
     return render(request, 'first_app/form.html', context=diction)
+
+
+
+
+# def form(request):
+#     new_form = forms.user_form()
+#     diction = {'test_form': new_form, 'heading1':"This form is creater with django library"}
+    
+#     if request.method == 'POST':
+#         new_form = forms.user_form(request.POST)
+
+#         if new_form.is_valid():
+#             user_name = new_form.cleaned_data['user_name']
+#             user_dob = new_form.cleaned_data['user_dob']
+#             user_email = new_form.cleaned_data['user_email']
+#             sex = new_form.cleaned_data['sex']
+#             language=new_form.cleaned_data['language']
+
+#             diction.update({'user_name':user_name})
+#             diction.update({'user_dob':user_dob})
+#             diction.update({'user_email':user_email})
+#             diction.update({'sex':sex})
+#             diction.update({'language':language})
+#             diction.update({'form_submitted':"Yest"})
+
+#     return render(request, 'first_app/form.html', context=diction)
