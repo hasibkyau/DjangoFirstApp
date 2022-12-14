f05b936f3985caecd0e8f0c02080f92ac6f6bdd2
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2022-12-14 12:46:26 +0600

    added mySql

--- My_First_Project/settings.py
+++ My_First_Project/settings.py
@@ -82,10 +82,22 @@ WSGI_APPLICATION = 'My_First_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.sqlite3',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_first_django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
