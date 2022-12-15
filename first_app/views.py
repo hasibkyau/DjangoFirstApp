81344201f0fc1f3cb04ff1b369b22af96f03653d
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2022-12-15 22:00:16 +0600

    delete record done

--- first_app/views.py
+++ first_app/views.py
@@ -65,12 +65,26 @@ def edit_album(request, album_id):
     album_info = Album.objects.get(pk=album_id)
     artist_id = album_info.artist_id
     form = forms.AlbumForm(instance=album_info)
+    diction = {}
 
     if request.method == 'POST':
         form = forms.AlbumForm(request.POST, instance=album_info)
-        if form.is_valid():
-            form.save()
-            return album_list(request, artist_id)
 
-    diction = {'edit_album':form, 'album_id': album_id, 'artist_id':artist_id}
+        if form.is_valid():
+            form.save(commit=True)
+            # return album_list(request, artist_id)
+            diction.update({'success_text':'Successfully Updated!'})
+
+    diction.update({'edit_album':form, 'album_id': album_id, 'artist_id':artist_id})
     return render(request, 'first_app/edit_album.html', context=diction)
+
+
+def delete_album(request, album_id):
+    album = Album.objects.get(pk = album_id).delete()
+    diction = {'delete_success':"Album Deleted Successfully!"}
+    return render(request, 'first_app/delete.html', context=diction)
+
+def delete_artist(request, artist_id):
+    artist = Musician.objects.get(pk = artist_id).delete()
+    diction = {'delete_success':"Artist Deleted Successfully!"}
+    return render(request, 'first_app/delete.html', context=diction)
